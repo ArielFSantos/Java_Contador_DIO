7320e5e1a7ff4441b1c3ec409045e23a7884ee16
Ariel Freitas <94645030+ArielFSantos@users.noreply.github.com>
2023-09-14 16:30:21 -0300

    criacao

---
+++ src/main/java/com/mycompany/java_contador_dio/Java_Contador_DIO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.java_contador_dio;
+
+/**
+ *
+ * @author ariel
+ */
+public class Java_Contador_DIO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
